c1825896d8ce9c1e6ced55e2ea8366027c189179
gurux01 <gurux@gurux.fi>
2016-11-15 11:25:46 +0200

    Version 1.0.9.1
    This version uses new version from Gurux.Common.

--- Development/Properties/AssemblyInfo.cs
+++ Development/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Gurux.SMS")]
@@ -14,12 +14,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("337d926e-45c2-452e-be6f-d0d7a3103220")]
-[assembly: AssemblyVersion("1.0.0.0")]//DO NOT CHANGE!
-[assembly: AssemblyFileVersion("1.0.8.1")]
+[assembly: AssemblyVersion("1.0.9.1")]
+[assembly: AssemblyFileVersion("1.0.9.1")]
